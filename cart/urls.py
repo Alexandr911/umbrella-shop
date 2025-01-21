71626002cae2769c975acd8e5f0b5bbfe99e53d4
Alex <alexander.duz912dmail.com>
2025-01-21 10:11:22 +0100

    Add cart_detail view and template

--- cart/urls.py
+++ cart/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import add_to_cart
+from .views import cart_detail
 
 urlpatterns = [
     path('add/<int:product_id>/', add_to_cart, name='add_to_cart'),
+    path('', cart_detail, name='cart_detail'),
 ]
--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import get_object_or_404, redirect
+from django.shortcuts import get_object_or_404, redirect, render
 from django.http import JsonResponse
 from .models import CartItem
 from products.models import Product
@@ -19,3 +19,18 @@ def add_to_cart(request, product_id):
     cart_item.save()
 
     return JsonResponse({"message": "Product added to cart", "quantity": cart_item.quantity})
+
+
+def cart_detail(request):
+    session_key = request.session.session_key
+    if not session_key:
+        request.session.create()
+        session_key = request.session.session_key
+
+    cart_items = CartItem.objects.filter(session_key=session_key)
+    total_price = sum(item.product.price * item.quantity for item in cart_items)
+
+    return render(request, 'cart/cart_detail.html', {
+        'cart_items': cart_items,
+        'total_price': total_price,
+    })
