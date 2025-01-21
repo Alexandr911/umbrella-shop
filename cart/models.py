e0b094f3905d8c887f43ae8a4f897af8a73f54b2
Alex <alexander.duz912dmail.com>
2025-01-21 10:03:46 +0100

    Add CartItem model for the shopping cart

--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from products.models import Product
 
+class CartItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+    session_key = models.CharField(max_length=255, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.quantity} x {self.product.name}"
 # Create your models here.
