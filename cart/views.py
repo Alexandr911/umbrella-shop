2171b3625063562b5c9bedac331ba91f0e191df3
Alex <alexander.duz912dmail.com>
2025-01-21 10:07:22 +0100

    Add add_to_cart view and routing

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect
+from django.http import JsonResponse
+from .models import CartItem
+from products.models import Product
 
-# Create your views here.
+def add_to_cart(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    session_key = request.session.session_key
+    if not session_key:
+        request.session.create()
+        session_key = request.session.session_key
+
+    cart_item, created = CartItem.objects.get_or_create(
+        product=product,
+        session_key=session_key
+    )
+    if not created:
+        cart_item.quantity += 1
+    cart_item.save()
+
+    return JsonResponse({"message": "Product added to cart", "quantity": cart_item.quantity})
